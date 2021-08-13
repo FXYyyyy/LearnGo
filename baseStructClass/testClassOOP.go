0cfb18f103ba2dcc2062c5fc494e18a3b00dab08
fangzhi <842938617@qq.com>
2021-08-13 17:58:23 +0800

    classTest

--- baseStructClass/testClassOOP.go
+++ baseStructClass/testClassOOP.go
@@ -1,5 +1,64 @@
 package main
 
+import "fmt"
+
 func main() {
-	$END$
+	a := Animal{"田园猫"}
+	p := Pet{"kk"}
+
+	//fmt.Println(a.getName())
+	fmt.Println(a.call())
+	fmt.Println(a.favorFood())
+
+	c := Cat{a, p}
+	fmt.Println(c.getName())
+	fmt.Println(c.call())
+	fmt.Println(c.favorFood())
 }
+
+//动物类
+type Animal struct {
+	name string
+}
+/*
+func (a Animal) getName() string {
+	return a.name
+}
+ */
+func (a Animal) call() string {
+	return "动物的叫声=="
+}
+func (a Animal) favorFood() string {
+	return "最爱吃的食物=="
+}
+
+//宠物类
+type Pet struct {
+	name string
+}
+
+func (p Pet) getName() string {
+	return p.name
+}
+
+//小猫，组合动物类、宠物类 ==== 继承
+type Cat struct {
+	Animal
+	Pet
+}
+
+//重构动物类的方法 ==== 多态
+//继承存在函数名字冲突时，会报错，需要组合的类重写
+func (c Cat) call() string {
+	return "喵喵喵"
+}
+
+func (c Cat) favorFood() string {
+	return "fish"
+}
+
+/*
+func (c Cat) getName() string {
+	return c.Animal.name + c.Pet.name
+}
+ */
--- baseStructClass/testCreateClass.go
+++ baseStructClass/testCreateClass.go
@@ -1,5 +1,70 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	$END$
+	stu1 := NewStudent(1, "zhizhi", true, 95.5)
+	fmt.Println(stu1.GetName())
+
+	fmt.Printf("%T", stu1)
+
+	stu1.setScore(90)	//stu1是一个指针类型，但是在使用时，会自动的解引用
+	fmt.Println(stu1)
+}
+
+type Student struct {
+	id uint
+	name string
+	male bool
+	score float64
+}
+
+/**
+* 初始化一个struct
+* NewStudent
+* @Description: 
+* @param id
+* @param name
+* @param male
+* @param score
+* @return *Student
+*/
+func NewStudent(id uint, name string, male bool, score float64) *Student {
+	return &Student{id, name, male, score}
+}
+
+/**
+* 获取一个名字（值方法）
+* GetName
+* @Description:
+* @receiver s
+* @return string
+*/
+func (s Student) GetName() string {
+	return s.name
+}
+
+/**
+* 设置分数（指针方法）
+* setScore
+* @Description:
+* @receiver s
+* @param newScore
+*/
+func (s *Student) setScore(newScore float64) {
+	s.score = newScore
+}
+
+/**
+* 以字符串格式打印类的实例，无需显式调用 String 方法，Go 语言会自动调用该方法来打印
+* String
+* @Description:
+* @receiver s
+* @return string
+*/
+func (s Student) String() string {
+	return fmt.Sprintf("{id: %d, name: %s, male: %t, score: %f}",
+		s.id, s.name, s.male, s.score)
 }
