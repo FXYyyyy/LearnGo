96ed0433b0cef61f42c4a87643ee633937b0398a
fangzhi <842938617@qq.com>
2021-08-19 22:58:46 +0800

    test pool

---
+++ corutine/testPool.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	//simplePool()
+	testPoolWithGroup()
+}
+
+/**
+* simplePool
+* @Description: 一个简单的临时对象池的使用
+*/
+func simplePool()  {
+	pool := sync.Pool{
+		New: func() interface{} {	//如果pool临时对象池里没有数据，则通过New方法，创建一个数据返回，保证始终有返回
+			return "Hell, Pool"
+		},
+	}
+
+	value := "zhizhi"	//
+	pool.Put(value)
+
+	fmt.Println(pool.Get())
+	fmt.Println(pool.Get())
+}
+
+/**
+* shareString
+* @Description: 利用pool，实现协程间的对象资源共享
+* @param pool
+* @param deferFunc
+*/
+func shareString(pool *sync.Pool, deferFunc func())  {
+	defer deferFunc()
+	str := "zhihzi"
+	pool.Put(str)	//将str存入pool，实现该协程的对象，与其他协程的共享
+}
+func testPoolWithGroup()  {
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	pool := sync.Pool{
+		New: func() interface{} {
+			return "aaa"
+		},
+	}
+
+	go shareString(&pool, wg.Done)
+	wg.Wait()
+
+	fmt.Println(pool.Get())	//获取子协程放入的对象
+}
